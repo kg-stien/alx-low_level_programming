94956731b9c200ff33b4ce992de3a3dc9e704358
kg-stien <marvunumadu@gmail.com>
2022-09-14 09:56:58 +0100

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *  print_alphabet - function to print abc
+ *
+ *  REturn:0
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c - 'a'; < <- 'z'; <++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
